27e2ffcd0a324dcd801655db54a7d3f6e3611512
Florian Schulze <florian.schulze@gmx.net>
2013-05-26 20:04:39 +0200

    Enable SD card adapter.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -311,7 +311,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 
 //LCD and SD support
 //#define ULTRA_LCD  //general lcd support, also 16x2
-//#define SDSUPPORT // Enable SD Card Support in Hardware Console
+#define SDSUPPORT // Enable SD Card Support in Hardware Console
 
 //#define ULTIMAKERCONTROLLER //as available from the ultimaker online store.
 //#define ULTIPANEL  //the ultipanel as on thingiverse
